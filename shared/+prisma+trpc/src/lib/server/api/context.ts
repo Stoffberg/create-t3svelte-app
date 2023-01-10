cdd598457faa0c42151b6eed57d645809dfeec1e
Dirk Beukes <dirk_beukes@hotmail.com>
2023-01-10 18:20:41 +0200

    fix: context import prism+trpc

--- shared/+prisma+trpc/src/lib/server/api/context.ts
+++ shared/+prisma+trpc/src/lib/server/api/context.ts
@@ -1,8 +1,7 @@
 import type { RequestEvent } from "@sveltejs/kit";
-import { type inferAsyncReturnType } from "@trpc/server";
+import type { inferAsyncReturnType } from "@trpc/server";
 
 import { prisma } from "$lib/server/db";
-import { t } from "./trpc";
 
 export const createContext = async (event: RequestEvent) => {
   return {
